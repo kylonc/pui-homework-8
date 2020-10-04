2c314cfae3cdb671e410937642a97b82090d1ed8
Kylon Chiang <kylonc@andrew.cmu.edu>
2020-10-04 00:05:32 -0400

    Making progress on templates

--- index.ts
+++ index.ts
@@ -1,20 +1,31 @@
 import express, { Express } from "express";
 import path from "path";
+import nunjucks from "nunjucks";
 
 const app: Express = express();
 const port: number = 8080;
 
 // Serve housekeeping
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join("public", "views"));
+app.set("view engine", "html")
+app.use(express.static("public"))
+
+// Configure nunjucks
+nunjucks.configure(app.get("views"), {
+    express: app
+});
 
 // Initialize server
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+    res.render("index");
+});
+
+app.get("/:route", (req, res) => {
+    res.render(req.params.route);
 });
 
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log("Listening on port ", port);
-
 });
 
